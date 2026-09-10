453723095f0b1a088ab1eb6589bfa003b8320048
wackzilla <wackzilla@gmail.com>
2026-09-10 14:41:18 -0400

    Change league name from 'Mexico' to 'Fútbol'

--- data.js
+++ data.js
@@ -5,7 +5,7 @@
 // ============================================================
 
 // The name shown at the top of the page.
-const LEAGUE_NAME = "Viva la Mexico";
+const LEAGUE_NAME = "Viva la Fútbol";
 
 // --------------------------------------------------------------
 // THIS WEEK'S ANNOUNCEMENT BANNER (fallback / default)
